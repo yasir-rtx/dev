2719effabeb0ca3f63048cdf768ff9cbdf8e75fd
yasir-rtx <yelfvankarlenstain@gmail.com>
2022-01-12 16:05:47 +0700

    Input

---
+++ C#/02-Input/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input kata : ");
+            string hi = Console.ReadLine();
+            Console.WriteLine("Your input : {0}", hi);
+        }
+    }
+}
