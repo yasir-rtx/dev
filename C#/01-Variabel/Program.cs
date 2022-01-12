b1bb517d986d0191a8b8a09c7cf0ec7ce04f52f3
yasir-rtx <yelfvankarlenstain@gmail.com>
2022-01-12 15:41:29 +0700

    Basic variable in c sharp

---
+++ C#/01-Variabel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01_Variabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int x = 6;             // Bilangan bulat
+           double phi = 3.14;     // Bilangan desimal
+           char gender = 'L';     // Satu karakter
+           string word = "Dream"; // Array dari char
+           bool status = true;    // True or False
+
+           Console.WriteLine(x);
+           Console.WriteLine(phi);
+           Console.WriteLine(gender);
+           Console.WriteLine(word);
+           Console.WriteLine(status); 
+        }
+    }
+}
