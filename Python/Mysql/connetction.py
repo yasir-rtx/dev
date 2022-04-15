33c685b5f13098efbde61fef3baed1d028d2d384
yasir-rtx <yelfvankarlenstain@gmail.com>
2022-04-15 18:16:16 +0700

    INSERT data

--- Python/Mysql/connetction.py
+++ Python/Mysql/connetction.py
@@ -12,8 +12,9 @@ db = mysql.connector.connect(
 # Creating 'cursor' as query executioner
 cursor = db.cursor()
 
+# %s == placeholder
 sql = "INSERT INTO members (kode, name, level) VALUES (%s, %s, %s)"
 val = ("Ghost", "Yelf", "Spesial")
 cursor.execute(sql, val)
-db.commit()
+db.commit() # SAVE DATA
 print ("{} data ditambahkan".format(cursor.rowcount))
